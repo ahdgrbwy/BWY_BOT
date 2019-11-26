26ce544703c79aabedb851e68e550cf328a326f5
ahmed <ahmed.essam1799@gmail.com>
2019-11-26 18:49:29 +0200

    Add feature _help, _info, _ping

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { config } = require("dotenv");
 
 const client = new Discord.Client({
    disableEveryone: true
-});
+})
 
-const prefix = "_";
+const prefix = "_"; // _command
 
 config({path: __dirname + "/.env"})
 
@@ -22,15 +22,44 @@ client.on("ready", () => {
 })
 
 client.on("message", async message => {
-   if (!message.content.startsWith(prefix) || message.author.bot) return;
+   if (message.author.bot) return
+   if (!message.guild) return
+   if (!message.content.startsWith(prefix)) return
 
+   const args = message.content.slice(prefix.length).split(/ +/),
+   cmd        = args.shift().toLowerCase()
 
-   const args = message.content.slice(prefix.length).split(/ +/);
-   const cmd  = args.shift().toLowerCase();
+   if (cmd === "help") {
+      const box = new Discord.RichEmbed()
+         .setColor("#0080ff")
+         .setTitle(
+            "> All bot commands: \n _ping \n _info"
+         )
+         
+      message.channel.send(box)
+   }
 
-   if (cmd === "hi") {
-      message.channel.send('Hello World')      
+   if (cmd === "ping") { // Ping Feature
+      const msg = await message.channel.send('testing...');
+      msg.edit(`> Your ping: ${Math.round(client.ping)}ms`);
+   }
+
+   if (cmd === "info") { // Informations feature
+      const box = new Discord.RichEmbed()
+         .setColor("#0080ff")
+         .setAuthor(message.guild.name, message.guild.iconURL)
+         .setDescription(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+         )
+         .addBlankField()
+         .addField('Website:', 'https://ahmedessam.info/', true)
+         .addField('IP Server MTASA:', 'mtasa://xxx.xx.xx.xx:xxxxx/', true)
+         .addField('Facebook:', 'https://www.facebook.com/ahdgrbwi/')
+         .addField('Youtube:', 'https://www.youtube.com/c/NitroNN/')
+         .setFooter('©Development by Ahmed Essam', message.guild.iconURL);
+
+      message.channel.send(box)
    }
 })
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN); // Change it in a file .env
