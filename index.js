940bd4bb0330c1a11d9b47df0f50d6e277f82225
ahmed <ahmed.essam1799@gmail.com>
2019-11-27 20:27:03 +0200

    install express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const { Client, RichEmbed } = require("discord.js");
 const { config } = require("dotenv");
 const moment = require("moment");
@@ -8,6 +9,16 @@ const pingFeature = require("./features/ping");
 const infoFeature = require("./features/info");
 const avatarFeature = require("./features/avatar");
 const userInfoFeature = require("./features/userInfo");
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const client = new Client({
    disableEveryone: true
